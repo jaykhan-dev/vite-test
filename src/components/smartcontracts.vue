609334138a6b82549692536e722bcbfcf96d3feb
Jay Khan <jaykhan.sound@gmail.com>
2022-01-09 02:43:30 -0500

    smart contract change

--- src/components/smartcontracts.vue
+++ src/components/smartcontracts.vue
@@ -1,26 +1,27 @@
 <template>
     <div class="flex justify-center dark:bg-black text-white dark:duration-300 p-2">
         <div class="border border-gray-700 rounded-lg bg-gradient-to-r from-gray-800 to-gray-900 p-4 shadow-2xl">
-            <div class="p-4 items-center">
-                <div class="p-2 items-center">
-                    <p class="uppercase">Smart contracts</p>
+            <div class="p-4 flex justify-center items-center">
+                <div class="p-2 grid place-items-center">
+                    <p class="uppercase bg-blue-500 p-2 shadow-2xl w-full text-center">Smart contracts</p>
                     <a href="" class="flex items-center p-2">
                         <img 
                             src="../assets/algo.png" 
                             alt=""
-                            width="50"
+                            width="100"
                             height=""
                             class="">
-                        <h1 class="font-bold p-2 lg:text-6xl text-4xl mt-8">Algorand</h1>                  
+                        <h1 class="font-bold p-2 lg:text-6xl text-4xl mt-8 -translate-y-2">Algorand</h1>                  
                     </a> 
                     
                     <h2 class="text-2xl mt-8">Written with PyTEAL because it's future proof</h2>
-                    <div class="">
+                    <div class="w-2/4">
                         <br>
                         • Python is among the most popular programming languages, along with JavaScript, Java, and C.
                         <br><br>
                         • The Blockchain is a radical shift in finance and trade, one which is disruptive, anarchistic, but also incredibly useful.
                     </div>
+                    
                 </div>
                 <div class="grid place-items-center py-8" id="smart-contract-bg">
                     
