e040947725028d0b1ee03c98f42b42ac19ba8087
Jay Khan <jaykhan.sound@gmail.com>
2022-01-16 22:12:44 -0500

    ?

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ import Mint from '../components/Mint.vue'
 import Search from '../components/Search.vue'
 import Marketplace from '../components/Marketplace.vue'
 import Wallet from '../components/Wallet.vue'
-import Nftpage from '../components/nftpage.vue'
+import Nftpage from '../components/Nftpage.vue'
 
 const routes = [
     { path: '/', name: 'Home', component: Home},
